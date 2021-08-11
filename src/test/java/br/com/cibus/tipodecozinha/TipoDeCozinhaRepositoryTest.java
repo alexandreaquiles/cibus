0c0383922e674c77d06b139a6138a39fa93f026e
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2021-08-11 19:08:10 -0300

    testes de TipoDeCozinhaRepository: existsByNome e existsByNomeWithDifferentId

--- src/test/java/br/com/cibus/tipodecozinha/TipoDeCozinhaRepositoryTest.java
+++ src/test/java/br/com/cibus/tipodecozinha/TipoDeCozinhaRepositoryTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class TipoDeCozinhaRepositoryTest {
 
@@ -33,6 +33,44 @@ class TipoDeCozinhaRepositoryTest {
         assertThat(tiposDeCozinha)
                 .extracting(TipoDeCozinha::getNome)
                 .containsExactly("Alemã", "Árabe", "Baiana", "Chinesa", "coreana", "Italiana", "Mexicana");
+    }
+
+    @Test
+    void deve_indicar_quando_o_nome_ja_existe() {
+        tipoDeCozinhaRepository.save(new TipoDeCozinha("Chilena"));
+
+        boolean jaExiste = tipoDeCozinhaRepository.existsByNome("Chilena");
+
+        assertThat(jaExiste).isTrue();
+    }
+
+    @Test
+    void deve_indicar_quando_o_nome_ainda_nao_existe() {
+        boolean jaExiste = tipoDeCozinhaRepository.existsByNome("Cozinha que não existe");
+
+        assertThat(jaExiste).isFalse();
+    }
+
+    @Test
+    void deve_indicar_quando_outro_tipo_de_cozinha_ja_tem_o_mesmo_nome() {
+        tipoDeCozinhaRepository.save(new TipoDeCozinha("Indiana"));
+        long outroIdQualquer = 999L;
+
+        boolean existeComOMesmoNomeMasComOutroId = tipoDeCozinhaRepository
+                .existsByNomeWithDifferentId("Indiana", outroIdQualquer);
+
+        assertThat(existeComOMesmoNomeMasComOutroId).isTrue();
+    }
+
+    @Test
+    void deve_indicar_quando_tipo_de_cozinha_com_o_mesmo_nome_tem_o_mesmo_id() {
+        TipoDeCozinha francesa = tipoDeCozinhaRepository.save(new TipoDeCozinha("Francesa"));
+        long mesmoId = francesa.getId();
+
+        boolean existeComOMesmoNomeMasComOutroId = tipoDeCozinhaRepository
+                .existsByNomeWithDifferentId("Francesa", mesmoId);
+
+        assertThat(existeComOMesmoNomeMasComOutroId).isFalse();
     }
 
 }
