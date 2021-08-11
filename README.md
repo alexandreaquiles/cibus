5537e8d8149da0cc3fea2c531dd1cfb3ea653669
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2021-08-11 12:56:54 -0300

    configurando flyway

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # cibus
+
+- Problema:
+
+criar e rodar scripts no BD
+
+criar (OK): createDatabaseIfNotExist=true
+rodar os scripts
+   spring.sql.init.mode = always (vai dar pau na segunda vez que vc rodar)
+            foi criado para BDs em memória
+            poderia dropar as tabelas no schema
+
+Eu quero manter scripts de evolução do meu BD.
+
+Migration
+
+https://databaserefactoring.com/
+
+versões do BD
+
+
---
+++ src/main/resources/db/migration/V3__cria-tabela-restaurante.sql
@@ -0,0 +1,13 @@
+create table restaurante (
+    id bigint auto_increment,
+    nome varchar(50) not null,
+    cep varchar(8) not null,
+    endereco varchar(200) not null,
+    cnpj varchar(14) not null,
+    descricao text,
+    tipo_de_cozinha_id bigint not null,
+    primary key (id),
+    unique key uk_restaurante_nome (nome),
+    key fk_restaurante_tipo_de_cozinha (tipo_de_cozinha_id),
+    constraint fk_restaurante_tipo_de_cozinha foreign key (tipo_de_cozinha_id) references tipo_de_cozinha(id)
+);
